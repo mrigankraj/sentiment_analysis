6123419ccdf778aa0c2091e0fa903e97617e030e
Mrigank Raj Dubey <97963422+mrigankraj@users.noreply.github.com>
2025-01-01 19:45:05 +0530

    App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SentimentDashboard } from './components/SentimentDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <SentimentDashboard />
+    </div>
+  );
+}
+
+export default App;
